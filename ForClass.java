d097b50b7958168beef80c1da59557147ccae6ac
joshuva1234 <36665146+joshuva1234@users.noreply.github.com>
2018-02-21 11:12:03 +0530

    Add files via upload

---
+++ ForClass.java
@@ -0,0 +1,10 @@
+package MyFirstPackage;
+
+public class ForClass {
+    int i=100;
+    int total=0;
+    int x=1;
+    for(x=1;x>=100;x++){
+        total=total+
+    }
+}
---
+++ HelloClass.java
@@ -0,0 +1,7 @@
+package MyFirstPackage;
+
+public class HelloClass {
+    public static void main(String args[]){
+      System.out.println("Hello joshuva george");
+    }
+}
